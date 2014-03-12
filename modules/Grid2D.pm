4744049fe9e1a0d20154e8c940f5d27c1c92dd2b
research@t-schmidt.name <research@t-schmidt.name@8294603f-69d7-dd3d-bfa7-16f95fb51b0c>
2014-03-12 16:09:43 +0000

    Extend the shifting value for the grid coordinates from 1 to 2 nm.

--- modules/Grid2D.pm
+++ modules/Grid2D.pm
@@ -39,7 +39,7 @@ my $gridYMax   = 0;
 my $gridDeltaX = 0.5;
 my $gridDeltaY = 0.5;
 
-my $coordShift = 1; # Shift all coordinates to avoid negative grid points (PBC).
+my $coordShift = 2; # Shift all coordinates to avoid negative grid points (PBC).
 
 
 
